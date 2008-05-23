ff59bedf7bf3f882a6b6d2b0804215077532b852
Walter Doerwald <walter@livinglogic.de>
2008-05-23 18:08:53 +0200

    Store the opcode as an enum. This requires less space and dispatching can be done via a switch statement.

--- library/src/com/livinglogic/sxtl/Renderer.java
+++ library/src/com/livinglogic/sxtl/Renderer.java
@@ -44,31 +44,26 @@ public class Renderer
 		for (int i = 0; i < codes.size(); ++i)
 		{
 			Template.Opcode code = codes.get(i);
-			if (code.name != null)
+			switch (code.name)
 			{
-				if (code.name.equals("if"))
-				{
+				case IF:
 					stack.add(i);
-				}
-				else if (code.name.equals("else"))
-				{
+					break;
+				case ELSE:
 					codes.get(stack.getLast()).jump = i;
 					stack.set(stack.size()-1, i);
-				}
-				else if (code.name.equals("endif"))
-				{
+					break;
+				case ENDIF:
 					codes.get(stack.getLast()).jump = i;
 					stack.removeLast();
-				}
-				else if (code.name.equals("for"))
-				{
+					break;
+				case FOR:
 					stack.add(i);
-				}
-				else if (code.name.equals("endfor"))
-				{
+					break;
+				case ENDFOR:
 					codes.get(stack.getLast()).jump = i;
 					stack.removeLast();
-				}
+					break;
 			}
 		}
 	}
@@ -87,341 +82,294 @@ public class Renderer
 		{
 			Template.Opcode code = codes.get(pc);
 
-			if (null == code.name)
+			switch (code.name)
 			{
-				output.append(code.location.getCode());
-			}
-			else if (code.name.equals("print"))
-			{
-				output.append(reg[code.r1]);
-			}
-			else if (code.name.equals("loadnone"))
-			{
-				reg[code.r1] = null;
-			}
-			else if (code.name.equals("loadfalse"))
-			{
-				reg[code.r1] = Boolean.FALSE;
-			}
-			else if (code.name.equals("loadtrue"))
-			{
-				reg[code.r1] = Boolean.TRUE;
-			}
-			else if (code.name.equals("loadstr"))
-			{
-				reg[code.r1] = code.arg;
-			}
-			else if (code.name.equals("loadint"))
-			{
-				reg[code.r1] = Integer.parseInt(code.arg);
-			}
-			else if (code.name.equals("loadfloat"))
-			{
-				reg[code.r1] = Double.parseDouble(code.arg);
-			}
-			else if (code.name.equals("loadvar"))
-			{
-				reg[code.r1] = variables.get(code.arg);
-			}
-			else if (code.name.equals("storevar"))
-			{
-				variables.put(code.arg, reg[code.r1]);
-			}
-			else if (code.name.equals("addvar"))
-			{
-				variables.put(code.arg, Utils.add(variables.get(code.arg), reg[code.r1]));
-			}
-			else if (code.name.equals("subvar"))
-			{
-				variables.put(code.arg, Utils.sub(variables.get(code.arg), reg[code.r1]));
-			}
-			else if (code.name.equals("mulvar"))
-			{
-				variables.put(code.arg, Utils.mul(variables.get(code.arg), reg[code.r1]));
-			}
-			else if (code.name.equals("truedivvar"))
-			{
-				variables.put(code.arg, Utils.truediv(variables.get(code.arg), reg[code.r1]));
-			}
-			else if (code.name.equals("floordivvar"))
-			{
-				variables.put(code.arg, Utils.floordiv(variables.get(code.arg), reg[code.r1]));
-			}
-			else if (code.name.equals("modvar"))
-			{
-				variables.put(code.arg, Utils.mod(variables.get(code.arg), reg[code.r1]));
-			}
-			else if (code.name.equals("delvar"))
-			{
-				variables.remove(code.arg);
-			}
-			else if (code.name.equals("for"))
-			{
-				Iterator<Object> iterator = Utils.iterator(reg[code.r2]);
-				if (iterator.hasNext())
-				{
-					reg[code.r1] = iterator.next();
-					iterators.add(new IteratorStackEntry(code.r1, pc, iterator));
-				}
-				else
-				{
+				case TEXT:
+					output.append(code.location.getCode());
+					break;
+				case PRINT:
+					output.append(reg[code.r1]);
+					break;
+				case LOADNONE:
+					reg[code.r1] = null;
+					break;
+				case LOADFALSE:
+					reg[code.r1] = Boolean.FALSE;
+					break;
+				case LOADTRUE:
+					reg[code.r1] = Boolean.TRUE;
+					break;
+				case LOADSTR:
+					reg[code.r1] = code.arg;
+					break;
+				case LOADINT:
+					reg[code.r1] = Integer.parseInt(code.arg);
+					break;
+				case LOADFLOAT:
+					reg[code.r1] = Double.parseDouble(code.arg);
+					break;
+				case LOADVAR:
+					reg[code.r1] = variables.get(code.arg);
+					break;
+				case STOREVAR:
+					variables.put(code.arg, reg[code.r1]);
+					break;
+				case ADDVAR:
+					variables.put(code.arg, Utils.add(variables.get(code.arg), reg[code.r1]));
+					break;
+				case SUBVAR:
+					variables.put(code.arg, Utils.sub(variables.get(code.arg), reg[code.r1]));
+					break;
+				case MULVAR:
+					variables.put(code.arg, Utils.mul(variables.get(code.arg), reg[code.r1]));
+					break;
+				case TRUEDIVVAR:
+					variables.put(code.arg, Utils.truediv(variables.get(code.arg), reg[code.r1]));
+					break;
+				case FLOORDIVVAR:
+					variables.put(code.arg, Utils.floordiv(variables.get(code.arg), reg[code.r1]));
+					break;
+				case MODVAR:
+					variables.put(code.arg, Utils.mod(variables.get(code.arg), reg[code.r1]));
+					break;
+				case DELVAR:
+					variables.remove(code.arg);
+					break;
+				case FOR:
+					Iterator<Object> iterator = Utils.iterator(reg[code.r2]);
+					if (iterator.hasNext())
+					{
+						reg[code.r1] = iterator.next();
+						iterators.add(new IteratorStackEntry(code.r1, pc, iterator));
+					}
+					else
+					{
+						pc = code.jump+1;
+						continue;
+					}
+					break;
+				case ENDFOR:
+					IteratorStackEntry entry = iterators.getLast();
+					if (entry.iterator.hasNext())
+					{
+						reg[entry.iteratorRegSpec] = entry.iterator.next();
+						pc = entry.pc;
+					}
+					else
+					{
+						iterators.removeLast();
+					}
+					break;
+				case IF:
+					if (!Utils.getBool(reg[code.r1]))
+					{
+						pc = code.jump+1;
+						continue;
+					}
+					break;
+				case ELSE:
 					pc = code.jump+1;
 					continue;
-				}
-			}
-			else if (code.name.equals("endfor"))
-			{
-				IteratorStackEntry entry = iterators.getLast();
-				if (entry.iterator.hasNext())
-				{
-					reg[entry.iteratorRegSpec] = entry.iterator.next();
-					pc = entry.pc;
-				}
-				else
-				{
-					iterators.removeLast();
-				}
-			}
-			else if (code.name.equals("if"))
-			{
-				if (!Utils.getBool(reg[code.r1]))
-				{
-					pc = code.jump+1;
-					continue;
-				}
-			}
-			else if (code.name.equals("else"))
-			{
-				pc = code.jump+1;
-				continue;
-			}
-			else if (code.name.equals("endif"))
-			{
-				//Skip to next opcode
-			}
-			else if (code.name.equals("getattr"))
-			{
-				reg[code.r1] = ((Map)reg[code.r2]).get(code.arg);
-			}
-			else if (code.name.equals("getitem"))
-			{
-				reg[code.r1] = Utils.getItem(reg[code.r2], reg[code.r3]);
-			}
-			else if (code.name.equals("getslice12"))
-			{
-				reg[code.r1] = Utils.getSlice(reg[code.r2], reg[code.r3], reg[code.r4]);
-			}
-			else if (code.name.equals("getslice1"))
-			{
-				reg[code.r1] = Utils.getSlice(reg[code.r2], reg[code.r3], null);
-			}
-			else if (code.name.equals("getslice2"))
-			{
-				reg[code.r1] = Utils.getSlice(reg[code.r2], null, reg[code.r4]);
-			}
-			else if (code.name.equals("getslice"))
-			{
-				reg[code.r1] = Utils.getSlice(reg[code.r2], null, null);
-			}
-			else if (code.name.equals("not"))
-			{
-				reg[code.r1] = Utils.getBool(reg[code.r2]) ? Boolean.FALSE : Boolean.TRUE;
-			}
-			else if (code.name.equals("equals"))
-			{
-				reg[code.r1] = Utils.equals(reg[code.r2], reg[code.r3]) ? Boolean.TRUE : Boolean.FALSE;
-			}
-			else if (code.name.equals("notequals"))
-			{
-				reg[code.r1] = Utils.equals(reg[code.r2], reg[code.r3]) ? Boolean.FALSE : Boolean.TRUE;
-			}
-			else if (code.name.equals("contains"))
-			{
-				reg[code.r1] = Utils.contains(reg[code.r2], reg[code.r3]) ? Boolean.TRUE : Boolean.FALSE;
-			}
-			else if (code.name.equals("notcontains"))
-			{
-				reg[code.r1] = Utils.contains(reg[code.r2], reg[code.r3]) ? Boolean.FALSE : Boolean.TRUE;
-			}
-			else if (code.name.equals("or"))
-			{
-				reg[code.r1] = (Utils.getBool(reg[code.r2]) || Utils.getBool(reg[code.r3])) ? Boolean.TRUE : Boolean.FALSE;
-			}
-			else if (code.name.equals("and"))
-			{
-				reg[code.r1] = (Utils.getBool(reg[code.r2]) && Utils.getBool(reg[code.r3])) ? Boolean.TRUE : Boolean.FALSE;
-			}
-			else if (code.name.equals("add"))
-			{
-				reg[code.r1] = Utils.add(reg[code.r2], reg[code.r3]);
-			}
-			else if (code.name.equals("sub"))
-			{
-				reg[code.r1] = Utils.sub(reg[code.r2], reg[code.r3]);
-			}
-			else if (code.name.equals("mul"))
-			{
-				reg[code.r1] = Utils.mul(reg[code.r2], reg[code.r3]);
-			}
-			else if (code.name.equals("truediv"))
-			{
-				reg[code.r1] = Utils.truediv(reg[code.r2], reg[code.r3]);
-			}
-			else if (code.name.equals("floordiv"))
-			{
-				reg[code.r1] = Utils.floordiv(reg[code.r2], reg[code.r3]);
-			}
-			else if (code.name.equals("mod"))
-			{
-				reg[code.r1] = Utils.mod(reg[code.r2], reg[code.r3]);
-			}
-			else if (code.name.equals("callfunc0"))
-			{
-				throw new RuntimeException("No function '" + code.arg + "' defined!");
-			}
-			else if (code.name.equals("callfunc1"))
-			{
-				if (code.arg.equals("xmlescape"))
-				{
-					reg[code.r1] = Utils.xmlescape(reg[code.r2]);
-				}
-				else if (code.arg.equals("str"))
-				{
-					reg[code.r1] = Utils.toString(reg[code.r2]);
-				}
-				else if (code.arg.equals("int"))
-				{
-					reg[code.r1] = Utils.toInteger(reg[code.r2]);
-				}
-				else if (code.arg.equals("len"))
-				{
-					reg[code.r1] = Utils.length(reg[code.r2]);
-				}
-				else if (code.arg.equals("enumerate"))
-				{
-					reg[code.r1] = Utils.enumerate(reg[code.r2]);
-				}
-				else if (code.arg.equals("isnone"))
-				{
-					reg[code.r1] = (null == reg[code.r2]) ? Boolean.TRUE : Boolean.FALSE;
-				}
-				else if (code.arg.equals("isstr"))
-				{
-					reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof String)) ? Boolean.TRUE : Boolean.FALSE;
-				}
-				else if (code.arg.equals("isint"))
-				{
-					reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Integer)) ? Boolean.TRUE : Boolean.FALSE;
-				}
-				else if (code.arg.equals("isfloat"))
-				{
-					reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Double)) ? Boolean.TRUE : Boolean.FALSE;
-				}
-				else if (code.arg.equals("isbool"))
-				{
-					reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Boolean)) ? Boolean.TRUE : Boolean.FALSE;
-				}
-				else if (code.arg.equals("islist"))
-				{
-					reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof List)) ? Boolean.TRUE : Boolean.FALSE;
-				}
-				else if (code.arg.equals("isdict"))
-				{
-					reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Map)) ? Boolean.TRUE : Boolean.FALSE;
-				}
-				else if (code.arg.equals("chr"))
-				{
-					reg[code.r1] = Utils.chr(reg[code.r2]);
-				}
-				else if (code.arg.equals("ord"))
-				{
-					reg[code.r1] = Utils.ord(reg[code.r2]);
-				}
-				else if (code.arg.equals("hex"))
-				{
-					reg[code.r1] = Utils.hex(reg[code.r2]);
-				}
-				else if (code.arg.equals("oct"))
-				{
-					reg[code.r1] = Utils.oct(reg[code.r2]);
-				}
-				else if (code.arg.equals("bin"))
-				{
-					reg[code.r1] = Utils.bin(reg[code.r2]);
-				}
-				else if (code.arg.equals("sorted"))
-				{
-					reg[code.r1] = Utils.sorted(reg[code.r2]);
-				}
-				else if (code.arg.equals("range"))
-				{
-					reg[code.r1] = Utils.range(reg[code.r2]);
-				}
-				else
-				{
+				case ENDIF:
+					//Skip to next opcode
+					break;
+				case GETATTR:
+					reg[code.r1] = ((Map)reg[code.r2]).get(code.arg);
+					break;
+				case GETITEM:
+					reg[code.r1] = Utils.getItem(reg[code.r2], reg[code.r3]);
+					break;
+				case GETSLICE12:
+					reg[code.r1] = Utils.getSlice(reg[code.r2], reg[code.r3], reg[code.r4]);
+					break;
+				case GETSLICE1:
+					reg[code.r1] = Utils.getSlice(reg[code.r2], reg[code.r3], null);
+					break;
+				case GETSLICE2:
+					reg[code.r1] = Utils.getSlice(reg[code.r2], null, reg[code.r4]);
+					break;
+				case GETSLICE:
+					reg[code.r1] = Utils.getSlice(reg[code.r2], null, null);
+					break;
+				case NOT:
+					reg[code.r1] = Utils.getBool(reg[code.r2]) ? Boolean.FALSE : Boolean.TRUE;
+					break;
+				case EQUALS:
+					reg[code.r1] = Utils.equals(reg[code.r2], reg[code.r3]) ? Boolean.TRUE : Boolean.FALSE;
+					break;
+				case NOTEQUALS:
+					reg[code.r1] = Utils.equals(reg[code.r2], reg[code.r3]) ? Boolean.FALSE : Boolean.TRUE;
+					break;
+				case CONTAINS:
+					reg[code.r1] = Utils.contains(reg[code.r2], reg[code.r3]) ? Boolean.TRUE : Boolean.FALSE;
+					break;
+				case NOTCONTAINS:
+					reg[code.r1] = Utils.contains(reg[code.r2], reg[code.r3]) ? Boolean.FALSE : Boolean.TRUE;
+					break;
+				case OR:
+					reg[code.r1] = (Utils.getBool(reg[code.r2]) || Utils.getBool(reg[code.r3])) ? Boolean.TRUE : Boolean.FALSE;
+					break;
+				case AND:
+					reg[code.r1] = (Utils.getBool(reg[code.r2]) && Utils.getBool(reg[code.r3])) ? Boolean.TRUE : Boolean.FALSE;
+					break;
+				case ADD:
+					reg[code.r1] = Utils.add(reg[code.r2], reg[code.r3]);
+					break;
+				case SUB:
+					reg[code.r1] = Utils.sub(reg[code.r2], reg[code.r3]);
+					break;
+				case MUL:
+					reg[code.r1] = Utils.mul(reg[code.r2], reg[code.r3]);
+					break;
+				case TRUEDIV:
+					reg[code.r1] = Utils.truediv(reg[code.r2], reg[code.r3]);
+					break;
+				case FLOORDIV:
+					reg[code.r1] = Utils.floordiv(reg[code.r2], reg[code.r3]);
+					break;
+				case MOD:
+					reg[code.r1] = Utils.mod(reg[code.r2], reg[code.r3]);
+					break;
+				case CALLFUNC0:
 					throw new RuntimeException("No function '" + code.arg + "' defined!");
-				}
-			}
-			else if (code.name.equals("callfunc2"))
-			{
-				if (code.arg.equals("range"))
-				{
-					reg[code.r1] = Utils.range(reg[code.r2], reg[code.r3]);
-				}
-				else
-				{
-					throw new RuntimeException("No function '" + code.arg + "' defined!");
-				}
-			}
-			else if (code.name.equals("callfunc3"))
-			{
-				if (code.arg.equals("range"))
-				{
-					reg[code.r1] = Utils.range(reg[code.r2], reg[code.r3], reg[code.r4]);
-				}
-				else
-				{
-					throw new RuntimeException("No function '" + code.arg + "' defined!");
-				}
-			}
-			else if (code.name.equals("callmeth0"))
-			{
-				if (code.arg.equals("split") || code.arg.equals("rsplit"))
-				{
-					reg[code.r1] = Utils.split(reg[code.r2]);
-				}
-				else if (code.arg.equals("strip"))
-				{
-					reg[code.r1] = Utils.strip(reg[code.r2]);
-				}
-				else if (code.arg.equals("lstrip"))
-				{
-					reg[code.r1] = Utils.lstrip(reg[code.r2]);
-				}
-				else if (code.arg.equals("rstrip"))
-				{
-					reg[code.r1] = Utils.rstrip(reg[code.r2]);
-				}
-				else if (code.arg.equals("upper"))
-				{
-					reg[code.r1] = Utils.upper(reg[code.r2]);
-				}
-				else if (code.arg.equals("lower"))
-				{
-					reg[code.r1] = Utils.lower(reg[code.r2]);
-				}
-				else if (code.arg.equals("items"))
-				{
-					reg[code.r1] = Utils.items(reg[code.r2]);
-				}
-				else
-				{
-					throw new RuntimeException("No method '" + code.arg + "' defined!");
-				}
-			}
-			else
-			{
-				throw new RuntimeException("Unknown opcode '" + code.name + "'!");
+				case CALLFUNC1:
+					if (code.arg.equals("xmlescape"))
+					{
+						reg[code.r1] = Utils.xmlescape(reg[code.r2]);
+					}
+					else if (code.arg.equals("str"))
+					{
+						reg[code.r1] = Utils.toString(reg[code.r2]);
+					}
+					else if (code.arg.equals("int"))
+					{
+						reg[code.r1] = Utils.toInteger(reg[code.r2]);
+					}
+					else if (code.arg.equals("len"))
+					{
+						reg[code.r1] = Utils.length(reg[code.r2]);
+					}
+					else if (code.arg.equals("enumerate"))
+					{
+						reg[code.r1] = Utils.enumerate(reg[code.r2]);
+					}
+					else if (code.arg.equals("isnone"))
+					{
+						reg[code.r1] = (null == reg[code.r2]) ? Boolean.TRUE : Boolean.FALSE;
+					}
+					else if (code.arg.equals("isstr"))
+					{
+						reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof String)) ? Boolean.TRUE : Boolean.FALSE;
+					}
+					else if (code.arg.equals("isint"))
+					{
+						reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Integer)) ? Boolean.TRUE : Boolean.FALSE;
+					}
+					else if (code.arg.equals("isfloat"))
+					{
+						reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Double)) ? Boolean.TRUE : Boolean.FALSE;
+					}
+					else if (code.arg.equals("isbool"))
+					{
+						reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Boolean)) ? Boolean.TRUE : Boolean.FALSE;
+					}
+					else if (code.arg.equals("islist"))
+					{
+						reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof List)) ? Boolean.TRUE : Boolean.FALSE;
+					}
+					else if (code.arg.equals("isdict"))
+					{
+						reg[code.r1] = ((null != reg[code.r2]) && (reg[code.r2] instanceof Map)) ? Boolean.TRUE : Boolean.FALSE;
+					}
+					else if (code.arg.equals("chr"))
+					{
+						reg[code.r1] = Utils.chr(reg[code.r2]);
+					}
+					else if (code.arg.equals("ord"))
+					{
+						reg[code.r1] = Utils.ord(reg[code.r2]);
+					}
+					else if (code.arg.equals("hex"))
+					{
+						reg[code.r1] = Utils.hex(reg[code.r2]);
+					}
+					else if (code.arg.equals("oct"))
+					{
+						reg[code.r1] = Utils.oct(reg[code.r2]);
+					}
+					else if (code.arg.equals("bin"))
+					{
+						reg[code.r1] = Utils.bin(reg[code.r2]);
+					}
+					else if (code.arg.equals("sorted"))
+					{
+						reg[code.r1] = Utils.sorted(reg[code.r2]);
+					}
+					else if (code.arg.equals("range"))
+					{
+						reg[code.r1] = Utils.range(reg[code.r2]);
+					}
+					else
+					{
+						throw new RuntimeException("No function '" + code.arg + "' defined!");
+					}
+					break;
+				case CALLFUNC2:
+					if (code.arg.equals("range"))
+					{
+						reg[code.r1] = Utils.range(reg[code.r2], reg[code.r3]);
+					}
+					else
+					{
+						throw new RuntimeException("No function '" + code.arg + "' defined!");
+					}
+					break;
+				case CALLFUNC3:
+					if (code.arg.equals("range"))
+					{
+						reg[code.r1] = Utils.range(reg[code.r2], reg[code.r3], reg[code.r4]);
+					}
+					else
+					{
+						throw new RuntimeException("No function '" + code.arg + "' defined!");
+					}
+					break;
+				case CALLMETH0:
+					if (code.arg.equals("split") || code.arg.equals("rsplit"))
+					{
+						reg[code.r1] = Utils.split(reg[code.r2]);
+					}
+					else if (code.arg.equals("strip"))
+					{
+						reg[code.r1] = Utils.strip(reg[code.r2]);
+					}
+					else if (code.arg.equals("lstrip"))
+					{
+						reg[code.r1] = Utils.lstrip(reg[code.r2]);
+					}
+					else if (code.arg.equals("rstrip"))
+					{
+						reg[code.r1] = Utils.rstrip(reg[code.r2]);
+					}
+					else if (code.arg.equals("upper"))
+					{
+						reg[code.r1] = Utils.upper(reg[code.r2]);
+					}
+					else if (code.arg.equals("lower"))
+					{
+						reg[code.r1] = Utils.lower(reg[code.r2]);
+					}
+					else if (code.arg.equals("items"))
+					{
+						reg[code.r1] = Utils.items(reg[code.r2]);
+					}
+					else
+					{
+						throw new RuntimeException("No method '" + code.arg + "' defined!");
+					}
+					break;
+				default:
+					throw new RuntimeException("Unknown opcode '" + code.name + "'!");
 			}
 			++pc;
 		}
