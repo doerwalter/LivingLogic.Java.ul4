c6d8caa498ea8c6004228b472d5c7ce85900fc96
Walter Doerwald <walter@livinglogic.de>
2008-05-24 12:53:50 +0200

    Port changes from XIST: Reuse compile() methods as much as possible.

--- library/src/com/livinglogic/sxtl/l4c.py
+++ library/src/com/livinglogic/sxtl/l4c.py
@@ -291,43 +291,28 @@ class Const(AST):
 	Common baseclass for all constants (used for type testing in constant folding)
 	"""
 
-
-class None_(Const):
-	type = "none"
-
 	def __repr__(self):
 		return "%s()" % self.__class__.__name__
 
 	def compile(self, template, registers, location):
 		r = allocreg(registers, location)
-		template.opcode("loadnone", r, location)
+		template.opcode("load%s" % self.type, r, location)
 		return r
+
+
+class None_(Const):
+	type = "none"
+	value = None
 
 
 class True_(Const):
 	type = "true"
 	value = True
 
-	def __repr__(self):
-		return "%s()" % self.__class__.__name__
-
-	def compile(self, template, registers, location):
-		r = allocreg(registers, location)
-		template.opcode("loadtrue", r, location)
-		return r
-
 
 class False_(Const):
 	type = "false"
 	value = False
-
-	def __repr__(self):
-		return "%s()" % self.__class__.__name__
-
-	def compile(self, template, registers, location):
-		r = allocreg(registers, location)
-		template.opcode("loadfalse", r, location)
-		return r
 
 
 class Value(Const):
@@ -415,22 +400,6 @@ class GetAttr(AST):
 		return r
 
 
-class GetItem(AST):
-	def __init__(self, obj, key):
-		self.obj = obj
-		self.key = key
-
-	def __repr__(self):
-		return "%s(%r, %r)" % (self.__class__.__name__, self.obj, self.key)
-
-	def compile(self, template, registers, location):
-		r1 = self.obj.compile(template, registers, location)
-		r2 = self.key.compile(template, registers, location)
-		template.opcode("getitem", r1, r1, r2, location)
-		freereg(registers, r2)
-		return r1
-
-
 class GetSlice12(AST):
 	def __init__(self, obj, index1, index2):
 		self.obj = obj
@@ -450,51 +419,6 @@ class GetSlice12(AST):
 		return r1
 
 
-class GetSlice1(AST):
-	def __init__(self, obj, index1):
-		self.obj = obj
-		self.index1 = index1
-
-	def __repr__(self):
-		return "%s(%r, %r)" % (self.__class__.__name__, self.obj, self.index1)
-
-	def compile(self, template, registers, location):
-		r1 = self.obj.compile(template, registers, location)
-		r2 = self.index1.compile(template, registers, location)
-		template.opcode("getslice1", r1, r1, r2, location)
-		freereg(registers, r2)
-		return r1
-
-
-class GetSlice2(AST):
-	def __init__(self, obj, index2):
-		self.obj = obj
-		self.index2 = index2
-
-	def __repr__(self):
-		return "%s(%r, %r)" % (self.__class__.__name__, self.obj, self.index2)
-
-	def compile(self, template, registers, location):
-		r1 = self.obj.compile(template, registers, location)
-		r2 = self.index2.compile(template, registers, location)
-		template.opcode("getslice2", r1, r1, r2, location)
-		freereg(registers, r2)
-		return r1
-
-
-class GetSlice(AST):
-	def __init__(self, obj):
-		self.obj = obj
-
-	def __repr__(self):
-		return "%s(%r)" % (self.__class__.__name__, self.obj)
-
-	def compile(self, template, registers, location):
-		r1 = self.obj.compile(template, registers, location)
-		template.opcode("getslice", r1, r1, location)
-		return r1
-
-
 class Unary(AST):
 	opcode = None
 
@@ -508,6 +432,10 @@ class Unary(AST):
 		r = self.obj.compile(template, registers, location)
 		template.opcode(self.opcode, r, r, location)
 		return r
+
+
+class GetSlice(Unary):
+	opcode = "getslice"
 
 
 class Not(Unary):
@@ -534,6 +462,18 @@ class Binary(AST):
 		template.opcode(self.opcode, r1, r1, r2, location)
 		freereg(registers, r2)
 		return r1
+
+
+class GetItem(Binary):
+	opcode = "getitem"
+
+
+class GetSlice1(Binary):
+	opcode = "getslice1"
+
+
+class GetSlice2(Binary):
+	opcode = "getslice2"
 
 
 class Equal(Binary):
