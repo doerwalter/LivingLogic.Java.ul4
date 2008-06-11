e35c812bd09989c8dd2f5f6bbb6a82bae7446500
Walter Doerwald <walter@livinglogic.de>
2008-06-11 15:11:32 +0200

    Fix name of the Python module.

--- library/src/com/livinglogic/ull/Compiler.java
+++ library/src/com/livinglogic/ull/Compiler.java
@@ -16,7 +16,7 @@ public class Compiler
 		props.setProperty("python.path", "C:\\jython\\Lib;C:\\ull");
 		PythonInterpreter.initialize(System.getProperties(), props, new String[] {""});
 		PythonInterpreter interpreter = new PythonInterpreter();
-		interpreter.exec("from l4c import Compiler");
+		interpreter.exec("from ullc import Compiler");
 		PyObject compilerclass = interpreter.get("Compiler");
 		PyObject compilerObj = compilerclass.__call__();
 		compiler = (CompilerType)compilerObj.__tojava__(CompilerType.class);
