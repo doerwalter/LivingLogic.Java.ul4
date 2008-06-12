6dccd095f103ff822840e1e0f84c6e1f2bf9d863
Walter Doerwald <walter@livinglogic.de>
2008-06-12 14:42:28 +0200

    Use linefeeds in the example, for testing line/column numbers in exceptions.

--- library/src/com/livinglogic/ull/Main.java
+++ library/src/com/livinglogic/ull/Main.java
@@ -13,7 +13,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		long start = new Date().getTime();
-		Template tmpl = Compiler.compile("<?if data?><ul><?for item in data?><li><?print xmlescape(item)?></li><?end for?></ul><?end if?>");
+		Template tmpl = Compiler.compile("<?if data?><ul>\n<?for item in data?><li><?print xmlescape(item)?></li>\n<?end for?></ul>\n<?end if?>");
 		System.out.println(tmpl.dumps());
 /*		Template linetmpl = factory.compile("<li><?print None?><?print False?><?print True?><?print 42?><?print 4.2?><?print 'gurk'?>(<?print xmlescape(data)?>)</li>");
 		System.out.println("compiled " + (time()-start));
