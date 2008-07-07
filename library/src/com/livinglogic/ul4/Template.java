26134bf2a52f39b870583aee9360647235b98dc7
Walter Doerwald <walter@livinglogic.de>
2008-07-07 14:53:22 +0200

    Bump version number for binary format.

--- library/src/com/livinglogic/ul4/Template.java
+++ library/src/com/livinglogic/ul4/Template.java
@@ -67,7 +67,7 @@ public class Template
 
 	public static final String HEADER = "ul4";
 
-	public static final String VERSION = "1";
+	public static final String VERSION = "2";
 
 	public String startdelim;
 
