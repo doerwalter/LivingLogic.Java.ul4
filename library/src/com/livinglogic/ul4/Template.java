9c6c9b9c6f951afee480b58377f74a9c1d6df02b
Walter Doerwald <walter@livinglogic.de>
2008-07-09 12:44:24 +0200

    Bump version number.

--- library/src/com/livinglogic/ul4/Template.java
+++ library/src/com/livinglogic/ul4/Template.java
@@ -88,7 +88,7 @@ public class Template
 	/**
 	 * The version number used in the compiled format of the template.
 	 */
-	public static final String VERSION = "2";
+	public static final String VERSION = "3";
 
 	/**
 	 * The start delimiter for tags (defaults to <code>&lt;?</code>)
