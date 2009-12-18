011be1b8bd3273331fd2eb6343efe0d31b92f4d5
Walter Doerwald <walter@livinglogic.de>
2009-12-18 16:55:25 +0100

    Update test.

--- library/src/com/livinglogic/ul4/Main.java
+++ library/src/com/livinglogic/ul4/Main.java
@@ -9,11 +9,11 @@ public class Main
 	public static void main(String[] args)
 	{
 		Long c = new Long(42);
-		Template tmpl = Compiler.compile("<?print 1/t?>");
+		Template tmpl = Compiler.compile("<?print t[1]?>");
 		System.out.println(tmpl);
 		long start = System.currentTimeMillis();
 		Map vars = new HashMap<String, Object>();
-		vars.put("t", 2);
+		vars.put("t", "123");
 		String output = tmpl.renders(vars);
 		System.out.println("rendered " + (System.currentTimeMillis()-start));
 		System.out.println(output);
