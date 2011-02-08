e90ca83317863d8f4e238bb25b419c4302176398
Walter Doerwald <walter@livinglogic.de>
2011-02-08 14:53:06 +0100

    Fix implementation of notequals operator.

--- library/src/com/livinglogic/ul4/Utils.java
+++ library/src/com/livinglogic/ul4/Utils.java
@@ -1336,7 +1336,7 @@ public class Utils
 	public static boolean ne(Object obj1, Object obj2)
 	{
 		if (null != obj1 && null != obj2)
-			return cmp(obj1, obj2, "!=") == 0;
+			return cmp(obj1, obj2, "!=") != 0;
 		return false;
 	}
 
