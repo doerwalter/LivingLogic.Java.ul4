5b2afdea77646ad634961c02d34d3559e145d98d
Andreas Gassner <ag@livinglogic.de>
2011-07-26 12:59:13 +0200

    ul4c.py and spark.py are now added with the jar command to ul4jython.jar and are placed in the root directory not in src/main/resources

--- makejar.sh
+++ makejar.sh
@@ -1,2 +1,6 @@
 cp jython.jar ul4jython.jar
-zip ul4jython.jar src/main/resources/spark.py src/main/resources/ul4c.py
+#zip ul4jython.jar src/main/resources/spark.py src/main/resources/ul4c.py
+
+jar uf ul4jython.jar -C src/main/resources spark.py
+jar uf ul4jython.jar -C src/main/resources ul4c.py
+
