01bc8fad3a6a8f2c4048746daf9d1ae2ed352255
Walter Doerwald <walter@livinglogic.de>
2012-04-05 16:16:18 +0200

    Put the mapping of opcode name to the integer code into two maps.

--- src/main/java/com/livinglogic/ul4/Opcode.java
+++ src/main/java/com/livinglogic/ul4/Opcode.java
@@ -212,148 +212,93 @@ public class Opcode extends ObjectAsMap
 	public int jump; // store the index of the "corresponding" opcode, e.g. the "endfor" opcode for a "for" opcode (set by InterpretedTemplate.annotate())
 	public InterpretedTemplate template; // if the opcode if a "def" opcode, store the subtemplate here (set by InterpretedTemplate.annotate())
 
+	private static HashMap<String, Integer> _name2code = new HashMap<String, Integer>();
+	private static HashMap<Integer, String> _code2name = new HashMap<Integer, String>();
+
+	private static void _mapName2Code(String name, int code)
+	{
+		_name2code.put(name, code);
+		_code2name.put(code, name);
+	}
+
+	static
+	{
+		_mapName2Code(null, OC_TEXT);
+		_mapName2Code("loadnone", OC_LOADNONE);
+		_mapName2Code("loadfalse", OC_LOADFALSE);
+		_mapName2Code("loadtrue", OC_LOADTRUE);
+		_mapName2Code("loadint", OC_LOADINT);
+		_mapName2Code("loadfloat", OC_LOADFLOAT);
+		_mapName2Code("loadstr", OC_LOADSTR);
+		_mapName2Code("loaddate", OC_LOADDATE);
+		_mapName2Code("loadcolor", OC_LOADCOLOR);
+		_mapName2Code("buildlist", OC_BUILDLIST);
+		_mapName2Code("builddict", OC_BUILDDICT);
+		_mapName2Code("addlist", OC_ADDLIST);
+		_mapName2Code("adddict", OC_ADDDICT);
+		_mapName2Code("updatedict", OC_UPDATEDICT);
+		_mapName2Code("loadvar", OC_LOADVAR);
+		_mapName2Code("storevar", OC_STOREVAR);
+		_mapName2Code("addvar", OC_ADDVAR);
+		_mapName2Code("subvar", OC_SUBVAR);
+		_mapName2Code("mulvar", OC_MULVAR);
+		_mapName2Code("truedivvar", OC_TRUEDIVVAR);
+		_mapName2Code("floordivvar", OC_FLOORDIVVAR);
+		_mapName2Code("modvar", OC_MODVAR);
+		_mapName2Code("delvar", OC_DELVAR);
+		_mapName2Code("getattr", OC_GETATTR);
+		_mapName2Code("getitem", OC_GETITEM);
+		_mapName2Code("getslice12", OC_GETSLICE12);
+		_mapName2Code("getslice1", OC_GETSLICE1);
+		_mapName2Code("getslice2", OC_GETSLICE2);
+		_mapName2Code("getslice", OC_GETSLICE);
+		_mapName2Code("print", OC_PRINT);
+		_mapName2Code("printx", OC_PRINTX);
+		_mapName2Code("not", OC_NOT);
+		_mapName2Code("neg", OC_NEG);
+		_mapName2Code("contains", OC_CONTAINS);
+		_mapName2Code("notcontains", OC_NOTCONTAINS);
+		_mapName2Code("eq", OC_EQ);
+		_mapName2Code("ne", OC_NE);
+		_mapName2Code("lt", OC_LT);
+		_mapName2Code("le", OC_LE);
+		_mapName2Code("gt", OC_GT);
+		_mapName2Code("ge", OC_GE);
+		_mapName2Code("add", OC_ADD);
+		_mapName2Code("sub", OC_SUB);
+		_mapName2Code("mul", OC_MUL);
+		_mapName2Code("floordiv", OC_FLOORDIV);
+		_mapName2Code("truediv", OC_TRUEDIV);
+		_mapName2Code("and", OC_AND);
+		_mapName2Code("or", OC_OR);
+		_mapName2Code("mod", OC_MOD);
+		_mapName2Code("callfunc0", OC_CALLFUNC0);
+		_mapName2Code("callfunc1", OC_CALLFUNC1);
+		_mapName2Code("callfunc2", OC_CALLFUNC2);
+		_mapName2Code("callfunc3", OC_CALLFUNC3);
+		_mapName2Code("callfunc4", OC_CALLFUNC4);
+		_mapName2Code("callmeth0", OC_CALLMETH0);
+		_mapName2Code("callmeth1", OC_CALLMETH1);
+		_mapName2Code("callmeth2", OC_CALLMETH2);
+		_mapName2Code("callmeth3", OC_CALLMETH3);
+		_mapName2Code("callmethkw", OC_CALLMETHKW);
+		_mapName2Code("if", OC_IF);
+		_mapName2Code("else", OC_ELSE);
+		_mapName2Code("endif", OC_ENDIF);
+		_mapName2Code("for", OC_FOR);
+		_mapName2Code("endfor", OC_ENDFOR);
+		_mapName2Code("break", OC_BREAK);
+		_mapName2Code("continue", OC_CONTINUE);
+		_mapName2Code("render", OC_RENDER);
+		_mapName2Code("def", OC_DEF);
+		_mapName2Code("enddef", OC_ENDDEF);
+	}
+
 	public static int name2code(String name)
 	{
-		if (name == null)
-			return OC_TEXT;
-		else if (name.equals("loadnone"))
-			return OC_LOADNONE;
-		else if (name.equals("loadfalse"))
-			return OC_LOADFALSE;
-		else if (name.equals("loadtrue"))
-			return OC_LOADTRUE;
-		else if (name.equals("loadint"))
-			return OC_LOADINT;
-		else if (name.equals("loadfloat"))
-			return OC_LOADFLOAT;
-		else if (name.equals("loadstr"))
-			return OC_LOADSTR;
-		else if (name.equals("loaddate"))
-			return OC_LOADDATE;
-		else if (name.equals("loadcolor"))
-			return OC_LOADCOLOR;
-		else if (name.equals("buildlist"))
-			return OC_BUILDLIST;
-		else if (name.equals("builddict"))
-			return OC_BUILDDICT;
-		else if (name.equals("addlist"))
-			return OC_ADDLIST;
-		else if (name.equals("adddict"))
-			return OC_ADDDICT;
-		else if (name.equals("updatedict"))
-			return OC_UPDATEDICT;
-		else if (name.equals("loadvar"))
-			return OC_LOADVAR;
-		else if (name.equals("storevar"))
-			return OC_STOREVAR;
-		else if (name.equals("addvar"))
-			return OC_ADDVAR;
-		else if (name.equals("subvar"))
-			return OC_SUBVAR;
-		else if (name.equals("mulvar"))
-			return OC_MULVAR;
-		else if (name.equals("truedivvar"))
-			return OC_TRUEDIVVAR;
-		else if (name.equals("floordivvar"))
-			return OC_FLOORDIVVAR;
-		else if (name.equals("modvar"))
-			return OC_MODVAR;
-		else if (name.equals("delvar"))
-			return OC_DELVAR;
-		else if (name.equals("getattr"))
-			return OC_GETATTR;
-		else if (name.equals("getitem"))
-			return OC_GETITEM;
-		else if (name.equals("getslice12"))
-			return OC_GETSLICE12;
-		else if (name.equals("getslice1"))
-			return OC_GETSLICE1;
-		else if (name.equals("getslice2"))
-			return OC_GETSLICE2;
-		else if (name.equals("getslice"))
-			return OC_GETSLICE;
-		else if (name.equals("print"))
-			return OC_PRINT;
-		else if (name.equals("printx"))
-			return OC_PRINTX;
-		else if (name.equals("not"))
-			return OC_NOT;
-		else if (name.equals("neg"))
-			return OC_NEG;
-		else if (name.equals("contains"))
-			return OC_CONTAINS;
-		else if (name.equals("notcontains"))
-			return OC_NOTCONTAINS;
-		else if (name.equals("eq"))
-			return OC_EQ;
-		else if (name.equals("ne"))
-			return OC_NE;
-		else if (name.equals("lt"))
-			return OC_LT;
-		else if (name.equals("le"))
-			return OC_LE;
-		else if (name.equals("gt"))
-			return OC_GT;
-		else if (name.equals("ge"))
-			return OC_GE;
-		else if (name.equals("add"))
-			return OC_ADD;
-		else if (name.equals("sub"))
-			return OC_SUB;
-		else if (name.equals("mul"))
-			return OC_MUL;
-		else if (name.equals("floordiv"))
-			return OC_FLOORDIV;
-		else if (name.equals("truediv"))
-			return OC_TRUEDIV;
-		else if (name.equals("and"))
-			return OC_AND;
-		else if (name.equals("or"))
-			return OC_OR;
-		else if (name.equals("mod"))
-			return OC_MOD;
-		else if (name.equals("callfunc0"))
-			return OC_CALLFUNC0;
-		else if (name.equals("callfunc1"))
-			return OC_CALLFUNC1;
-		else if (name.equals("callfunc2"))
-			return OC_CALLFUNC2;
-		else if (name.equals("callfunc3"))
-			return OC_CALLFUNC3;
-		else if (name.equals("callfunc4"))
-			return OC_CALLFUNC4;
-		else if (name.equals("callmeth0"))
-			return OC_CALLMETH0;
-		else if (name.equals("callmeth1"))
-			return OC_CALLMETH1;
-		else if (name.equals("callmeth2"))
-			return OC_CALLMETH2;
-		else if (name.equals("callmeth3"))
-			return OC_CALLMETH3;
-		else if (name.equals("callmethkw"))
-			return OC_CALLMETHKW;
-		else if (name.equals("if"))
-			return OC_IF;
-		else if (name.equals("else"))
-			return OC_ELSE;
-		else if (name.equals("endif"))
-			return OC_ENDIF;
-		else if (name.equals("for"))
-			return OC_FOR;
-		else if (name.equals("endfor"))
-			return OC_ENDFOR;
-		else if (name.equals("break"))
-			return OC_BREAK;
-		else if (name.equals("continue"))
-			return OC_CONTINUE;
-		else if (name.equals("render"))
-			return OC_RENDER;
-		else if (name.equals("def"))
-			return OC_DEF;
-		else if (name.equals("enddef"))
-			return OC_ENDDEF;
-		else
-			throw new IllegalArgumentException("Opcode name " + name + " unknown!");
+		if (_name2code.containsKey(name))
+			return _name2code.get(name);
+		throw new IllegalArgumentException("Opcode name " + name + " unknown!");
 	}
 
 	public static int callfunc0name2code(String name)
@@ -634,144 +579,9 @@ public class Opcode extends ObjectAsMap
 
 	public static String code2name(int code)
 	{
-		if (code == OC_TEXT)
-			return null;
-		else if (code == OC_LOADNONE)
-			return "loadnone";
-		else if (code == OC_LOADFALSE)
-			return "loadfalse";
-		else if (code == OC_LOADTRUE)
-			return "loadtrue";
-		else if (code == OC_LOADINT)
-			return "loadint";
-		else if (code == OC_LOADFLOAT)
-			return "loadfloat";
-		else if (code == OC_LOADSTR)
-			return "loadstr";
-		else if (code == OC_LOADDATE)
-			return "loaddate";
-		else if (code == OC_BUILDLIST)
-			return "buildlist";
-		else if (code == OC_BUILDDICT)
-			return "builddict";
-		else if (code == OC_ADDLIST)
-			return "addlist";
-		else if (code == OC_ADDDICT)
-			return "adddict";
-		else if (code == OC_UPDATEDICT)
-			return "updatedict";
-		else if (code == OC_LOADVAR)
-			return "loadvar";
-		else if (code == OC_STOREVAR)
-			return "storevar";
-		else if (code == OC_ADDVAR)
-			return "addvar";
-		else if (code == OC_SUBVAR)
-			return "subvar";
-		else if (code == OC_MULVAR)
-			return "mulvar";
-		else if (code == OC_TRUEDIVVAR)
-			return "truedivvar";
-		else if (code == OC_FLOORDIVVAR)
-			return "floordivvar";
-		else if (code == OC_MODVAR)
-			return "modvar";
-		else if (code == OC_DELVAR)
-			return "delvar";
-		else if (code == OC_GETATTR)
-			return "getattr";
-		else if (code == OC_GETITEM)
-			return "getitem";
-		else if (code == OC_GETSLICE12)
-			return "getslice12";
-		else if (code == OC_GETSLICE1)
-			return "getslice1";
-		else if (code == OC_GETSLICE2)
-			return "getslice2";
-		else if (code == OC_GETSLICE)
-			return "getslice";
-		else if (code == OC_PRINT)
-			return "print";
-		else if (code == OC_PRINTX)
-			return "printx";
-		else if (code == OC_NOT)
-			return "not";
-		else if (code == OC_NEG)
-			return "neg";
-		else if (code == OC_EQ)
-			return "eq";
-		else if (code == OC_NE)
-			return "ne";
-		else if (code == OC_LT)
-			return "lt";
-		else if (code == OC_LE)
-			return "le";
-		else if (code == OC_GT)
-			return "gt";
-		else if (code == OC_GE)
-			return "ge";
-		else if (code == OC_CONTAINS)
-			return "contains";
-		else if (code == OC_NOTCONTAINS)
-			return "notcontains";
-		else if (code == OC_ADD)
-			return "add";
-		else if (code == OC_SUB)
-			return "sub";
-		else if (code == OC_MUL)
-			return "mul";
-		else if (code == OC_FLOORDIV)
-			return "floordiv";
-		else if (code == OC_TRUEDIV)
-			return "truediv";
-		else if (code == OC_AND)
-			return "and";
-		else if (code == OC_OR)
-			return "or";
-		else if (code == OC_MOD)
-			return "mod";
-		else if (code == OC_CALLFUNC0)
-			return "callfunc0";
-		else if (code == OC_CALLFUNC1)
-			return "callfunc1";
-		else if (code == OC_CALLFUNC2)
-			return "callfunc2";
-		else if (code == OC_CALLFUNC3)
-			return "callfunc3";
-		else if (code == OC_CALLFUNC4)
-			return "callfunc4";
-		else if (code == OC_CALLMETH0)
-			return "callmeth0";
-		else if (code == OC_CALLMETH1)
-			return "callmeth1";
-		else if (code == OC_CALLMETH2)
-			return "callmeth2";
-		else if (code == OC_CALLMETH3)
-			return "callmeth3";
-		else if (code == OC_CALLMETHKW)
-			return "callmethkw";
-		else if (code == OC_IF)
-			return "if";
-		else if (code == OC_ELSE)
-			return "else";
-		else if (code == OC_ENDIF)
-			return "endif";
-		else if (code == OC_FOR)
-			return "for";
-		else if (code == OC_ENDFOR)
-			return "endfor";
-		else if (code == OC_BREAK)
-			return "break";
-		else if (code == OC_CONTINUE)
-			return "continue";
-		else if (code == OC_RENDER)
-			return "render";
-		else if (code == OC_DEF)
-			return "def";
-		else if (code == OC_ENDDEF)
-			return "enddef";
-		else
-			throw new IllegalArgumentException("Opcode code " + code + " unknown!");
+		if (_code2name.containsKey(code))
+			return _code2name.get(code);
+		throw new IllegalArgumentException("Opcode code " + code + " unknown!");
 	}
 
 	public Opcode(String name, int r1, int r2, int r3, int r4, int r5, String arg, Location location)
