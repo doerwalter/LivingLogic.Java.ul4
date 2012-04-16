478c22c58e386ace4b5acd93bd4b48500e7a0264
Walter Doerwald <walter@livinglogic.de>
2012-04-16 17:07:21 +0200

    Remove code that was used by the Jython parser.

--- makejar.sh
+++
@@ -1,7 +0,0 @@
-cp $HOME/.m2/repository/org/python/jython-standalone/2.5.2/jython-standalone-2.5.2.jar ul4jython.jar
-#cp jython.jar ul4jython.jar
-#zip ul4jython.jar src/main/resources/spark.py src/main/resources/ul4c.py
-
-jar uf ul4jython.jar -C src/main/resources spark.py
-jar uf ul4jython.jar -C src/main/resources ul4c.py
-
