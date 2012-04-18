05ab5fa56698ad213084eadf71b122894a60f642
Walter Doerwald <walter@livinglogic.de>
2012-04-18 17:08:33 +0200

    Handle missing indexes in GetSlice.

--- src/main/java/com/livinglogic/ul4/GetSlice.java
+++ src/main/java/com/livinglogic/ul4/GetSlice.java
@@ -35,7 +35,7 @@ public class GetSlice extends AST
 
 	public Object evaluate(EvaluationContext context) throws IOException
 	{
-		return Utils.getSlice(obj.evaluate(context), index1.evaluate(context), index2.evaluate(context));
+		return Utils.getSlice(obj.evaluate(context), index1 != null ? index1.evaluate(context) : null, index2 != null ? index2.evaluate(context) : null);
 	}
 
 	private static Map<String, ValueMaker> valueMakers = null;
