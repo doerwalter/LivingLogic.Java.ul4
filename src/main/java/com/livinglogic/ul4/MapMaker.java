2ecb2b0f384540fb52f08e2782eb84e613488b31
Walter Doerwald <walter@livinglogic.de>
2012-05-07 12:02:05 +0200

    Add class MapMaker.
    This class will be used for creating a Map in Map literals.

---
+++ src/main/java/com/livinglogic/ul4/MapMaker.java
@@ -0,0 +1,38 @@
+/*
+** Copyright 2009-2012 by LivingLogic AG, Bayreuth/Germany
+** All Rights Reserved
+** See LICENSE for the license
+*/
+
+package com.livinglogic.ul4;
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+/**
+ * Utility class for creating a Map object
+ *
+ * @author W. Dörwald, A. Gaßner
+ */
+public class MapMaker
+{
+	private Map map = new HashMap();
+
+	public MapMaker add(Object key, object value)
+	{
+		map.put(key, value);
+		return this;
+	}
+
+	public MapMaker add(Map map)
+	{
+		this.map.putAll(map);
+		return this;
+	}
+
+	public Map getMap()
+	{
+		return map;
+	}
+}
