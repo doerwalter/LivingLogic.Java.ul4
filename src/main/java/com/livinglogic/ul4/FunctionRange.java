4a0ce5876664252c9c0c61cd2dfc7d0d608a8f4a
Walter Doerwald <walter@livinglogic.de>
2012-06-18 22:33:19 +0200

    Reorder methods.

--- src/main/java/com/livinglogic/ul4/FunctionRange.java
+++ src/main/java/com/livinglogic/ul4/FunctionRange.java
@@ -10,6 +10,41 @@ import java.util.AbstractList;
 
 public class FunctionRange implements Function
 {
+	public String getName()
+	{
+		return "range";
+	}
+
+	public Object evaluate(EvaluationContext context, Object... args)
+	{
+		switch (args.length)
+		{
+			case 1:
+				return call(args[0]);
+			case 2:
+				return call(args[0], args[1]);
+			case 3:
+				return call(args[0], args[1], args[2]);
+			default:
+				throw new ArgumentCountMismatchException("function", "range", args.length, 1, 3);
+		}
+	}
+
+	public static Object call(Object obj)
+	{
+		return new Range(0, Utils.toInt(obj), 1);
+	}
+
+	public static Object call(Object obj1, Object obj2)
+	{
+		return new Range(Utils.toInt(obj1), Utils.toInt(obj2), 1);
+	}
+
+	public static Object call(Object obj1, Object obj2, Object obj3)
+	{
+		return new Range(Utils.toInt(obj1), Utils.toInt(obj2), Utils.toInt(obj3));
+	}
+
 	private static class Range extends AbstractList
 	{
 		int start;
@@ -63,40 +98,5 @@ public class FunctionRange implements Function
 		{
 			return length;
 		}
-	}
-
-	public static Object call(Object obj)
-	{
-		return new Range(0, Utils.toInt(obj), 1);
-	}
-
-	public static Object call(Object obj1, Object obj2)
-	{
-		return new Range(Utils.toInt(obj1), Utils.toInt(obj2), 1);
-	}
-
-	public static Object call(Object obj1, Object obj2, Object obj3)
-	{
-		return new Range(Utils.toInt(obj1), Utils.toInt(obj2), Utils.toInt(obj3));
-	}
-
-	public Object evaluate(EvaluationContext context, Object... args)
-	{
-		switch (args.length)
-		{
-			case 1:
-				return call(args[0]);
-			case 2:
-				return call(args[0], args[1]);
-			case 3:
-				return call(args[0], args[1], args[2]);
-			default:
-				throw new ArgumentCountMismatchException("function", "range", args.length, 1, 3);
-		}
-	}
-
-	public String getName()
-	{
-		return "range";
 	}
 }
