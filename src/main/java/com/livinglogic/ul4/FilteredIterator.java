e6b03b3f628b17d6ac5b506f61bc7477ac2d23c4
Walter Doerwald <walter@livinglogic.de>
2012-10-12 18:05:26 +0200

    Add abstract class FilteredIterator.
    This is used for implementing generator expressions in compiled mode.

---
+++ src/main/java/com/livinglogic/ul4/FilteredIterator.java
@@ -0,0 +1,51 @@
+/*
+** Copyright 2009-2012 by LivingLogic AG, Bayreuth/Germany
+** All Rights Reserved
+** See LICENSE for the license
+*/
+
+package com.livinglogic.ul4;
+
+import java.util.Iterator;
+
+public abstract class FilteredIterator implements Iterator
+{
+	protected Object nextItem;
+	protected boolean hasNextItem;
+
+	public FilteredIterator()
+	{
+		fetchNext();
+	}
+
+	abstract protected void fetchNext();
+
+	protected void haveNextItem(Object item)
+	{
+		hasNextItem = true;
+		nextItem = item;
+	}
+
+	protected void noNextItem()
+	{
+		hasNextItem = false;
+		nextItem = null;
+	}
+
+	public boolean hasNext()
+	{
+		return hasNextItem;
+	}
+
+	public Object next()
+	{
+		Object result = nextItem;
+		fetchNext();
+		return result;
+	}
+
+	public void remove()
+	{
+		throw new UnsupportedOperationException();
+	}
+}
