52e286e6a26cba965a01cd0298b0e72a68511d28
Walter Doerwald <walter@livinglogic.de>
2013-01-02 13:29:46 +0100

    Add class MapChain.

---
+++ src/main/java/com/livinglogic/utils/MapChain.java
@@ -0,0 +1,302 @@
+/*
+** Copyright 2009-2013 by LivingLogic AG, Bayreuth/Germany
+** All Rights Reserved
+** See LICENSE for the license
+*/
+
+package com.livinglogic.utils;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+/**
+ * A {@code Map} implemention that forwards all operation to one or two mapps.
+ * When a key or entry isn't found in the first map the second ("chained")
+ * {@code Map} is consulted.
+ *
+ * Methods of {@code MapChain} that modify the mapping only modify the first
+ * {@code Map} not the second one. Modifying the {@code MapChain} via the sets
+ * returned by {@code keySet()} or{@code entrySet()} is forbidden (i.e. those
+ * methods throw a {@code UnsupportedOperationException} exception).
+ *
+ * @author W. Doerwald
+ */
+public class MapChain<K, V> implements Map<K, V>
+{
+	class KeySet extends AbstractSet<K>
+	{
+		class KeySetIterator implements Iterator<K>
+		{
+			private Iterator<K> firstIterator;
+			private Iterator<K> secondIterator;
+			private boolean haveBufferedKey;
+			private K bufferedKey;
+
+			public KeySetIterator()
+			{
+				firstIterator = first.keySet().iterator();
+				secondIterator = second.keySet().iterator();
+				haveBufferedKey = false;
+				bufferedKey = null;
+			}
+
+			public boolean hasNext()
+			{
+				if (haveBufferedKey)
+					return true;
+				if (firstIterator.hasNext())
+					return true;
+
+				// the first iterator is exhausted; use the second one
+				// ignore all keys that the first iterator has already produced
+				while (secondIterator.hasNext())
+				{
+					K item = secondIterator.next();
+					// Is this a key we haven't seen before?
+					if (!first.containsKey(item))
+					{
+						haveBufferedKey = true;
+						bufferedKey = item;
+						return true;
+					}
+				}
+				return false;
+			}
+
+			public K next()
+			{
+				if (haveBufferedKey)
+				{
+					K result = bufferedKey;
+					bufferedKey = null;
+					haveBufferedKey = false;
+					return result;
+				}
+				if (firstIterator.hasNext())
+					return firstIterator.next();
+
+				// the first iterator is exhausted; use the second one
+				// ignore all keys that the first iterator has already produced
+				while (secondIterator.hasNext())
+				{
+					K item = secondIterator.next();
+					// Is this a key we haven't seen before?
+					if (!first.containsKey(item))
+						return item;
+				}
+				throw new NoSuchElementException();
+			}
+
+			public void remove()
+			{
+				throw new UnsupportedOperationException();
+			}
+		}
+
+		public boolean contains(Object o)
+		{
+			return containsKey(o);
+		}
+
+		public boolean containsAll(Collection<?> c)
+		{
+			for (Object obj: c)
+			{
+				if (!containsKey(obj))
+					return false;
+			}
+			return true;
+		}
+
+		public Iterator<K> iterator()
+		{
+			return new KeySetIterator();
+		}
+
+		public boolean isEmpty()
+		{
+			return MapChain.this.isEmpty();
+		}
+
+		public int size()
+		{
+			return MapChain.this.size();
+		}
+	}
+
+	class EntrySet extends AbstractSet<Map.Entry<K, V>>
+	{
+		class EntrySetIterator implements Iterator<Map.Entry<K, V>>
+		{
+			private Iterator<Map.Entry<K, V>> firstIterator;
+			private Iterator<Map.Entry<K, V>> secondIterator;
+			private Map.Entry<K, V> bufferedEntry;
+
+			public EntrySetIterator()
+			{
+				firstIterator = first.entrySet().iterator();
+				secondIterator = second.entrySet().iterator();
+				bufferedEntry = null;
+			}
+
+			public boolean hasNext()
+			{
+				if (bufferedEntry != null)
+					return true;
+				if (firstIterator.hasNext())
+					return true;
+
+				// the first iterator is exhausted; use the second one
+				// ignore all entries that the first iterator has already produced
+				while (secondIterator.hasNext())
+				{
+					Map.Entry<K, V> entry = secondIterator.next();
+					// Is this an entry we haven't seen before?
+					if (!first.containsKey(entry.getKey()))
+					{
+						bufferedEntry = entry;
+						return true;
+					}
+				}
+				return false;
+			}
+
+			public Map.Entry<K, V> next()
+			{
+				if (bufferedEntry != null)
+				{
+					Map.Entry<K, V> result = bufferedEntry;
+					bufferedEntry = null;
+					return result;
+				}
+				if (firstIterator.hasNext())
+					return firstIterator.next();
+
+				// the first iterator is exhausted; use the second one
+				// ignore all entries that the first iterator has already produced
+				while (secondIterator.hasNext())
+				{
+					Map.Entry<K, V> entry = secondIterator.next();
+					// Is this an entry we haven't seen before?
+					if (!first.containsKey(entry.getKey()))
+						return entry;
+				}
+				throw new NoSuchElementException();
+			}
+
+			public void remove()
+			{
+				throw new UnsupportedOperationException();
+			}
+		}
+
+		public Iterator<Map.Entry<K, V>> iterator()
+		{
+			return new EntrySetIterator();
+		}
+
+		public boolean isEmpty()
+		{
+			return MapChain.this.isEmpty();
+		}
+
+		public int size()
+		{
+			return MapChain.this.size();
+		}
+	}
+
+	private Map<K, V> first;
+	private Map<K, V> second;
+
+	public MapChain(Map<K, V> first, Map<K, V> second)
+	{
+		this.first = first;
+		this.second = second;
+	}
+
+	public boolean containsKey(Object key)
+	{
+		boolean containsKey = first.containsKey(key);
+		if (!containsKey)
+			containsKey = second.containsKey(key);
+		return containsKey;
+	}
+
+	public boolean containsValue(Object value)
+	{
+		boolean containsValue = first.containsValue(value);
+		if (!containsValue)
+			containsValue = second.containsValue(value);
+		return containsValue;
+	}
+
+	public Set<Map.Entry<K,V>> entrySet()
+	{
+		return new EntrySet();
+	}
+
+	public V get(Object key)
+	{
+		V value = first.get(key);
+		if (value == null && !first.containsKey(key))
+			value = second.get(key);
+		return value;
+	}
+
+	public boolean isEmpty()
+	{
+		boolean isEmpty = first.isEmpty();
+		if (isEmpty)
+			isEmpty = second.isEmpty();
+		return isEmpty;
+	}
+
+	public Set<K> keySet()
+	{
+		return new KeySet();
+	}
+
+	public int size()
+	{
+		int size = first.size();
+
+		for (Object key : second.keySet())
+		{
+			if (!first.containsKey(key))
+				++size;
+		}
+		return size;
+	}
+
+	public void clear()
+	{
+		first.clear();
+	}
+
+	public V put(K key, V value)
+	{
+		return first.put(key, value);
+	}
+
+	public V remove(Object key)
+	{
+		return first.remove(key);
+	}
+
+	public void putAll(Map<? extends K,? extends V> m)
+	{
+		first.putAll(m);
+	}
+
+	// FIXME: Implement this
+	public Collection<V> values()
+	{
+		throw new UnsupportedOperationException();
+	}
+}
