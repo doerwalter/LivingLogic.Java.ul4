fbfcae9eccabfd3cf87d862541ba6d4084828129
Walter Doerwald <walter@livinglogic.de>
2013-01-02 13:38:34 +0100

    Bump copyright year.

--- src/main/java/com/livinglogic/ul4/FunctionUTCNow.java
+++ src/main/java/com/livinglogic/ul4/FunctionUTCNow.java
@@ -1,5 +1,5 @@
 /*
-** Copyright 2009-2012 by LivingLogic AG, Bayreuth/Germany
+** Copyright 2009-2013 by LivingLogic AG, Bayreuth/Germany
 ** All Rights Reserved
 ** See LICENSE for the license
 */
