dbf37bf97a666692b7ecd93b9ae4f6bdd2c8de4b
Walter Doerwald <walter@livinglogic.de>
2017-02-24 15:00:35 +0100

    Bump copyright year.

--- src/main/java/com/livinglogic/ul4/UL4SetItem.java
+++ src/main/java/com/livinglogic/ul4/UL4SetItem.java
@@ -1,5 +1,5 @@
 /*
-** Copyright 2012-2016 by LivingLogic AG, Bayreuth/Germany
+** Copyright 2012-2017 by LivingLogic AG, Bayreuth/Germany
 ** All Rights Reserved
 ** See LICENSE for the license
 */
--- src/main/java/com/livinglogic/utils/AbstractCombiningMapChain.java
+++ src/main/java/com/livinglogic/utils/AbstractCombiningMapChain.java
@@ -1,5 +1,5 @@
 /*
-** Copyright 2009-2014 by LivingLogic AG, Bayreuth/Germany
+** Copyright 2009-2017 by LivingLogic AG, Bayreuth/Germany
 ** All Rights Reserved
 ** See LICENSE for the license
 */
