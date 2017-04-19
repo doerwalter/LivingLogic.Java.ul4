97c9845041b112fe0e6304d68105646d7902988c
Walter Doerwald <walter@livinglogic.de>
2017-04-19 11:14:58 +0200

    Store object and key of an AttributeException.

--- src/main/java/com/livinglogic/ul4/AttributeException.java
+++ src/main/java/com/livinglogic/ul4/AttributeException.java
@@ -15,8 +15,23 @@ package com.livinglogic.ul4;
  */
 public class AttributeException extends RuntimeException
 {
+	protected Object object;
+	protected Object key;
+
 	public AttributeException(Object object, Object key)
 	{
 		super(Utils.formatMessage("Object of type {!t} has no attribute {!r}!", object, key));
+		this.object = object;
+		this.key = key;
+	}
+
+	public Object getObject()
+	{
+		return object;
+	}
+
+	public Object getKey()
+	{
+		return key;
 	}
 }
