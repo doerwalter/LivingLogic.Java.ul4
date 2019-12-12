35a15a443919c99dbae98ab0d564d8f8a6ae3558
Walter Doerwald <walter@livinglogic.de>
2019-12-12 15:38:24 +0100

    Bump UL4 version number.

--- src/main/java/com/livinglogic/ul4/InterpretedTemplate.java
+++ src/main/java/com/livinglogic/ul4/InterpretedTemplate.java
@@ -42,7 +42,7 @@ public class InterpretedTemplate extends BlockAST implements UL4Name, UL4CallWit
 	/**
 	 * The version number used in the UL4ON dump of the template.
 	 */
-	public static final String VERSION = "48";
+	public static final String VERSION = "49";
 
 	/**
 	 * The name of the template/function (defaults to {@code null})
