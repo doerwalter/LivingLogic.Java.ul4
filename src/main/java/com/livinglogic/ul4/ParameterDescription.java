aa6ede29eeded9034aef5a05bdf95c4714a090ae
Walter Doerwald <walter@livinglogic.de>
2021-02-26 17:10:29 +0100

    Whitespace.

--- src/main/java/com/livinglogic/ul4/ParameterDescription.java
+++ src/main/java/com/livinglogic/ul4/ParameterDescription.java
@@ -16,33 +16,40 @@ public class ParameterDescription implements UL4Repr
 	The name of the parameter.
 	**/
 	protected String name;
+
 	/**
 	The position of the parameter in the signature.
 	**/
 	protected int position;
+
 	public enum Type
 	{
 		/**
 		The parameter must have an argument in the call.
 		**/
 		REQUIRED,
+
 		/**
 		The argument is optional. The parameter has a default value.
 		**/
 		DEFAULT,
+
 		/**
 		The parameter collects all additional positional arguments in a list.
 		**/
+
 		VAR_POSITIONAL,
 		/**
 		The parameter collects all additional keyword arguments in a map.
 		**/
 		VAR_KEYWORD,
 	}
+
 	/**
 	The type of the parameter
 	**/
 	protected Type type;
+
 	/**
 	The default value of the parameter (if it is of type {@code DEFAULT})
 	**/
