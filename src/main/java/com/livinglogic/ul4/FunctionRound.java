ab70c532f1e5e539584afad241c44b9c0f52163e
Walter Doerwald <walter@livinglogic.de>
2021-04-22 16:38:46 +0200

    Split round() implementation into multiple static methods.

--- src/main/java/com/livinglogic/ul4/FunctionRound.java
+++ src/main/java/com/livinglogic/ul4/FunctionRound.java
@@ -9,6 +9,7 @@ package com.livinglogic.ul4;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+
 public class FunctionRound extends Function
 {
 	@Override
@@ -31,146 +32,121 @@ public class FunctionRound extends Function
 		return call(args.get(0), args.get(1));
 	}
 
+	public static Object call(int x, int digits)
+	{
+		if (digits >= 0)
+			return x;
+		else // digits < 0
+		{
+			for (int i = 0; i < -digits; ++i)
+			{
+				if (i == -digits -1)
+					x += x < 0 ? -5 : 5;
+				x /= 10;
+			}
+			for (int i = 0; i < -digits; ++i)
+				x *= 10;
+			return x;
+		}
+	}
+
+	public static Object call(long x, int digits)
+	{
+		if (digits >= 0)
+			return x;
+		else // digits < 0
+		{
+			for (int i = 0; i < -digits; ++i)
+			{
+				if (i == -digits -1)
+					x += x < 0 ? -5 : 5;
+				x /= 10;
+			}
+			for (int i = 0; i < -digits; ++i)
+				x *= 10;
+			return x;
+		}
+	}
+
+	public static Object call(double x, int digits)
+	{
+		if (digits == 0)
+		{
+			x = Math.round(x);
+			if (Integer.MIN_VALUE <= x && x <= Integer.MAX_VALUE)
+				return (int)x;
+			else if (Long.MIN_VALUE <= x && x <= Long.MAX_VALUE)
+				return (long)x;
+			else
+				return new BigDecimal(x).toBigInteger();
+		}
+		else if (digits < 0)
+		{
+			for (int i = 0; i < -digits; ++i)
+				x /= 10.;
+			x = Math.round(x);
+			for (int i = 0; i < -digits; ++i)
+				x *= 10.;
+			if (Integer.MIN_VALUE <= x && x <= Integer.MAX_VALUE)
+				return (int)x;
+			else if (Long.MIN_VALUE <= x && x <= Long.MAX_VALUE)
+				return (long)x;
+			else
+				return new BigDecimal(x).toBigInteger();
+		}
+		else // digits > 0
+		{
+			for (int i = 0; i < digits; ++i)
+				x *= 10.;
+			x = Math.round(x);
+			for (int i = 0; i < digits; ++i)
+				x /= 10.;
+			return x;
+		}
+	}
+
+	public static BigInteger call(BigInteger x, int digits)
+	{
+		if (digits >= 0)
+			return x;
+		BigInteger offset = Utils.powerOfTen(-digits-1);
+		return x.divide(offset).add(new BigInteger(x.signum() >= 0 ? "5" : "-5")).divide(BigInteger.TEN).multiply(offset).multiply(BigInteger.TEN);
+	}
+
+	public static Object call(BigDecimal x, int digits)
+	{
+		if (digits != 0)
+			x = x.movePointRight(digits);
+		x = x.add(new BigDecimal(x.signum() >= 0 ? "0.5" : "-0.5"));
+		if (digits <= 0)
+		{
+			BigInteger intValue = x.toBigInteger();
+			if (digits < 0)
+				intValue = intValue.multiply(Utils.powerOfTen(-digits));
+			return intValue;
+		}
+		else
+			return new BigDecimal(x.toBigInteger().toString()).movePointLeft(digits);
+	}
+
+	public static Object call(Object x, int digits)
+	{
+		if (x instanceof Byte || x instanceof Short || x instanceof Integer)
+			return call(((Number)x).intValue(), digits);
+		else if (x instanceof Long)
+			return call(((Number)x).longValue(), digits);
+		else if (x instanceof Float || x instanceof Double)
+			return call(((Number)x).doubleValue(), digits);
+		else if (x instanceof BigInteger)
+			return call((BigInteger)x, digits);
+		else if (x instanceof BigDecimal)
+			return call((BigDecimal)x, digits);
+		throw new ArgumentTypeMismatchException("round({!t}, {!t}) not supported", x, digits);
+	}
+
 	public static Object call(Object x, Object digits)
 	{
-		int intDigits = Utils.toInt(digits);
-
-		if (x instanceof Byte || x instanceof Short || x instanceof Integer)
-		{
-			if (intDigits >= 0)
-				return x;
-			else // intDigits < 0
-			{
-				int value = ((Number)x).intValue();
-				for (int i = 0; i < -intDigits; ++i)
-				{
-					if (i == -intDigits -1)
-						value += value < 0 ? -5 : 5;
-					value /= 10;
-				}
-				for (int i = 0; i < -intDigits; ++i)
-					value *= 10;
-				return value;
-			}
-		}
-		else if (x instanceof Long)
-		{
-			if (intDigits >= 0)
-				return x;
-			else // intDigits < 0
-			{
-				long value = ((Number)x).longValue();
-				for (int i = 0; i < -intDigits; ++i)
-				{
-					if (i == -intDigits -1)
-						value += value < 0 ? -5 : 5;
-					value /= 10;
-				}
-				for (int i = 0; i < -intDigits; ++i)
-					value *= 10;
-				return value;
-			}
-		}
-		else if (x instanceof Float)
-		{
-			float value = ((Number)x).floatValue();
-			if (intDigits == 0)
-			{
-				value = Math.round(value);
-				if (Integer.MIN_VALUE <= value && value <= Integer.MAX_VALUE)
-					return (int)value;
-				else if (Long.MIN_VALUE <= value && value <= Long.MAX_VALUE)
-					return (long)value;
-				else
-					return new BigDecimal(value).toBigInteger();
-			}
-			else if (intDigits < 0)
-			{
-				for (int i = 0; i < -intDigits; ++i)
-					value /= 10.;
-				value = Math.round(value);
-				for (int i = 0; i < -intDigits; ++i)
-					value *= 10.;
-				if (Integer.MIN_VALUE <= value && value <= Integer.MAX_VALUE)
-					return (int)value;
-				else if (Long.MIN_VALUE <= value && value <= Long.MAX_VALUE)
-					return (long)value;
-				else
-					return new BigDecimal(value).toBigInteger();
-			}
-			else // intDigits > 0
-			{
-				for (int i = 0; i < intDigits; ++i)
-					value *= 10.;
-				value = Math.round(value);
-				for (int i = 0; i < intDigits; ++i)
-					value /= 10.;
-				return value;
-			}
-		}
-		else if (x instanceof Double)
-		{
-			double value = ((Number)x).doubleValue();
-			if (intDigits == 0)
-			{
-				value = Math.round(value);
-				if (Integer.MIN_VALUE <= value && value <= Integer.MAX_VALUE)
-					return (int)value;
-				else if (Long.MIN_VALUE <= value && value <= Long.MAX_VALUE)
-					return (long)value;
-				else
-					return new BigDecimal(value).toBigInteger();
-			}
-			else if (intDigits < 0)
-			{
-				for (int i = 0; i < -intDigits; ++i)
-					value /= 10.;
-				value = Math.round(value);
-				for (int i = 0; i < -intDigits; ++i)
-					value *= 10.;
-				if (Integer.MIN_VALUE <= value && value <= Integer.MAX_VALUE)
-					return (int)value;
-				else if (Long.MIN_VALUE <= value && value <= Long.MAX_VALUE)
-					return (long)value;
-				else
-					return new BigDecimal(value).toBigInteger();
-			}
-			else // intDigits > 0
-			{
-				for (int i = 0; i < intDigits; ++i)
-					value *= 10.;
-				value = Math.round(value);
-				for (int i = 0; i < intDigits; ++i)
-					value /= 10.;
-				return value;
-			}
-		}
-		else if (x instanceof BigInteger)
-		{
-			if (intDigits >= 0)
-				return x;
-			BigInteger offset = Utils.powerOfTen(-intDigits-1);
-			int signum = ((BigInteger)x).signum();
-			return ((BigInteger)x).divide(offset).add(new BigInteger(signum >= 0 ? "5" : "-5")).divide(BigInteger.TEN).multiply(offset).multiply(BigInteger.TEN);
-		}
-		else if (x instanceof BigDecimal)
-		{
-			int signum = ((BigDecimal)x).signum();
-			BigDecimal decValue = (BigDecimal)x;
-			if (intDigits != 0)
-				decValue = decValue.movePointRight(intDigits);
-			decValue = decValue.add(new BigDecimal(signum >= 0 ? "0.5" : "-0.5"));
-			if (intDigits <= 0)
-			{
-				BigInteger intValue = decValue.toBigInteger();
-				if (intDigits < 0)
-					intValue = intValue.multiply(Utils.powerOfTen(-intDigits));
-				return intValue;
-			}
-			else
-				return new BigDecimal(decValue.toBigInteger().toString()).movePointLeft(intDigits);
-		}
-		throw new ArgumentTypeMismatchException("round({!t}, {!t}) not supported", x, digits);
+		return call(x, Utils.toInt(digits));
 	}
 
 	public static final Function function = new FunctionRound();
