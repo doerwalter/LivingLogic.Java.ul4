20d95be494e1d090417c61cef818947d8ac9d071
Walter Doerwald <walter@livinglogic.de>
2021-04-22 14:18:37 +0200

    Make operator.attrgetter a type.

--- src/main/java/com/livinglogic/ul4/OperatorAttrGetter.java
+++ src/main/java/com/livinglogic/ul4/OperatorAttrGetter.java
@@ -12,21 +12,101 @@ import java.util.ArrayList;
 import org.apache.commons.lang3.StringUtils;
 
 
-public class OperatorAttrGetter extends Function
+public class OperatorAttrGetter extends Function implements UL4Instance, UL4Repr, UL4GetAttr, UL4Dir
 {
-	private List<String[]> allAttrNames;
-
-	public OperatorAttrGetter(String... attrNames)
+	protected static class Type extends AbstractInstanceType
 	{
-		allAttrNames = new ArrayList<String[]>();
-		for (String attrName : attrNames)
-			allAttrNames.add(StringUtils.splitByWholeSeparatorPreserveAllTokens(attrName, "."));
+		@Override
+		public String getModuleName()
+		{
+			return "operator";
+		}
+
+		@Override
+		public String getNameUL4()
+		{
+			return "attrgetter";
+		}
+
+		@Override
+		public String getDoc()
+		{
+			return "A callable object that fetches the given attribute(s) from its operand.";
+		}
+
+		@Override
+		public boolean instanceCheck(Object object)
+		{
+			return object instanceof OperatorAttrGetter;
+		}
+
+		private static final Signature signature = new Signature().addVarPositional("attrs");
+
+		@Override
+		public Signature getSignature()
+		{
+			return signature;
+		}
+
+		@Override
+		public Object create(BoundArguments args)
+		{
+			List arg = (List)args.get(0);
+
+			int argCount = arg.size();
+			if (argCount == 0)
+				throw new ArgumentCountMismatchException("type", "attrgetter", argCount, 1, -1);
+
+			for (Object attrName : arg)
+			{
+				if (!(attrName instanceof String))
+					throw new ArgumentTypeMismatchException(getFullNameUL4() + " arguments must be strings");
+			}
+
+			return new OperatorAttrGetter(arg);
+		}
+	}
+
+	public static final UL4Type type = new Type();
+
+	@Override
+	public UL4Type getTypeUL4()
+	{
+		return type;
+	}
+
+	@Override
+	public String getModuleName()
+	{
+		return "operator";
 	}
 
 	@Override
 	public String getNameUL4()
 	{
-		return "attrgetter";
+		StringBuilder buffer = new StringBuilder();
+		boolean first = true;
+		buffer.append("attrgetter(");
+
+		for (String[] attrNames : allAttrNames)
+		{
+			if (first)
+				first = false;
+			else
+				buffer.append(", ");
+			buffer.append(FunctionRepr.call(BoundStringMethodJoin.call(".", attrNames)));
+		}
+		buffer.append(")");
+		return buffer.toString();
+	}
+
+	private List<String[]> allAttrNames;
+
+	public OperatorAttrGetter(List<String> attrNames)
+	{
+		allAttrNames = new ArrayList<String[]>();
+		for (String attrName : attrNames)
+			allAttrNames.add(StringUtils.splitByWholeSeparatorPreserveAllTokens(attrName, "."));
 	}
 
 	private static final Signature signature = new Signature().addPositionalOnly("obj");
