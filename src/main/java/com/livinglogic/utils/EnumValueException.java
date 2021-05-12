73d2973d89bae3952761ddaf721d07fe65fb4f79
Walter Doerwald <walter@livinglogic.de>
2021-05-12 10:56:23 +0200

    Add missing source code.

---
+++ src/main/java/com/livinglogic/utils/EnumValueException.java
@@ -0,0 +1,17 @@
+/*
+** Copyright 2015-2021 by LivingLogic AG, Bayreuth/Germany
+** All Rights Reserved
+** See LICENSE for the license
+*/
+
+package com.livinglogic.utils;
+
+import com.livinglogic.ul4.FunctionRepr;
+
+public class EnumValueException extends RuntimeException
+{
+	public EnumValueException(String enumName, String value)
+	{
+		super("Value " + FunctionRepr.call(value) + " not supported from " + enumName);
+	}
+}
