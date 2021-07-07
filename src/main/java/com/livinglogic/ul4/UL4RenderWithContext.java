692eca5138b7b6d25530bcef74ef14802956dd16
Walter Doerwald <walter@livinglogic.de>
2021-07-07 13:58:40 +0200

    Add docstrings to UL4RenderWithContext.

--- src/main/java/com/livinglogic/ul4/UL4RenderWithContext.java
+++ src/main/java/com/livinglogic/ul4/UL4RenderWithContext.java
@@ -9,7 +9,22 @@ package com.livinglogic.ul4;
 import java.util.List;
 import java.util.Map;
 
+/**
+<p>Implementing the {@code UL4Render} interface makes an object renderable
+from UL4 (i.e. it can be used in the {@code render}, {@code renderx},
+{@code renderblock} and {@code renderblocks} tags).</p>
+
+<p>Since rendering always outputs the the {@link EvaluationContext} there is
+no method with one.</p>
+**/
 public interface UL4RenderWithContext
 {
+	/**
+	<p>Render this object.</p>
+
+	@param context The evaluation context.
+	@param args Positional arguments.
+	@param kwargs Keyword arguments.
+	**/
 	void renderUL4(EvaluationContext context, List<Object> args, Map<String, Object> kwargs);
 }
