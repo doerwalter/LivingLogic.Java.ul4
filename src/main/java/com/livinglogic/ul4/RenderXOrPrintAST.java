e98142a52477c45a6b32189c3c4f695f7a9a46e1
Walter Doerwald <walter@livinglogic.de>
2023-02-10 20:24:13 +0100

    Bump copyright year.

--- src/main/java/com/livinglogic/ul4/RenderXOrPrintAST.java
+++ src/main/java/com/livinglogic/ul4/RenderXOrPrintAST.java
@@ -1,5 +1,5 @@
 /*
-** Copyright 2009-2022 by LivingLogic AG, Bayreuth/Germany
+** Copyright 2009-2023 by LivingLogic AG, Bayreuth/Germany
 ** All Rights Reserved
 ** See LICENSE for the license
 */
